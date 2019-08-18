a0725a41026659321f29a86b4a5df0d0b71b9a6f
Wang Fan <wfrobin@163.com>
2019-08-18 13:45:29 +0800

    Initial Commit

---
+++ leetcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leetcode
+//
+//  Created by Wang Fan on 2019/8/18.
+//  Copyright © 2019 Wang Fan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
